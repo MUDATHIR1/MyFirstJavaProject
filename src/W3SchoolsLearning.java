a0a13cc82a1a3ebdb47423c7a8d2cd27edebcb0f
MUDATHIR1 <salaudeenmudathir1@gmail.com>
2023-09-05 13:57:46 +0300

    Initial commit

---
+++ src/W3SchoolsLearning.java
@@ -0,0 +1,44 @@
+public class W3SchoolsLearning {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! ");   // I am using this to test my single line comment 
+		// Still testing if the single line comment can work when used after each other 
+	/* 
+	 
+	 The double line comment is to be tested here as well lets see if this works without printing out the line in 
+	 between the Multi line comment 
+	 if it actually did not print it out, then the multi line comment works in my code and also it is nice using 
+	 it as it would be useful for explaining the code and giving comments
+	
+	 */
+	System.out.println("The double line comment is to be tested here as well + lets see if this works without printing out the line in between the Multi line comment if it actually did not print it out, then the multi line comment works in my code and \r\n"
+			+ "also it is nice using it as it would be useful for explaining the code and giving comments");
+	String myName = "Salaudeen Mudathir ";
+	int number1 = 225;
+	float num = 22.25f;
+	System.out.println(myName);
+	System.out.println(number1);
+	System.out.println(num);
+	
+	String FirstName = "Mudathir ";
+			String SecondName = "Salaudeen ";
+			String LastName = "Oluwafemi";
+			String FullName = FirstName  +  SecondName + LastName;
+			
+			System.out.println(FullName);
+			
+			String Firstname = "Mudathir1 ", Secondname = "Salaudeen ", Lastname = "Oluwafemi";
+			String Fullname = Firstname  +  Secondname + Lastname;
+			System.out.println(Fullname);
+			
+			int Modulus = 25 % 3;
+			System.out.println(Modulus);
+int Increment = 2;
+System.out.println(Increment);
+			
+
+
+
+	}
+	
+} 
